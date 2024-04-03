2effbbea0c9a8fcf01321dd76f5052b7361c9ebc
AbrahamAlgorithm <nightscholar0@gmail.com>
2024-04-03 03:28:17 +0200

    Index

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,22 @@
 #!/usr/bin/python3
 
 from api.v1.views import app_views  # Import the blueprint
+from flask import jsonify
+import models
 
-@app_views.route('/status')
-def index():
+@app_views.route('/status, strict_slashes=False')
+def get_status():
     """Returns a JSON response with "status": "OK"."""
     return jsonify({'status': 'OK'})
+
+@app_views.route('/stats')
+def status_page():
+    """return the status of each model"""
+    model = models.storage
+    ret = {'amenities': model.count("Amenity"),
+           'cities': model.count("City"),
+           'places': model.count("Place"),
+           'reviews': model.count("Review"),
+           'states': model.count("State"),
+           'users': model.count("User")}
+    return jsonify(ret)
